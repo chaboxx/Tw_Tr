35b721dafa841dd153c67de19f0475726b7d3998
RODRIGO CUEVA PASTOR <rcpchaboxx>
2023-07-22 19:38:25 -0500

    feat: firts commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="bg-current">
+      <h1 className="text-red-400">hola</h1>
+      <h1 className="text-red-400">hola</h1>
+
+      <div>
+        <button className="b">Hola Mundo</button>
+      </div>
+    </div>
+  );
+}
